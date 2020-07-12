fd3707a4741f304e280d0dbbee3a5d98679f93fe
yuyumi <changyu2000@gmail.com>
2020-07-11 23:20:01 -0500

    some more cases

--- covid_conway.py
+++ covid_conway.py
@@ -191,26 +191,28 @@ def simulate(case,
         time += 1
     return total_infected_time, current_infected_time, time_list, population
 
+# Plot the simulation using the parameters
 def plotSimulation(total_infected_time, current_infected_time, time_list, population):
     plt.scatter(time_list, current_infected_time)
     plt.scatter(time_list, total_infected_time)
     #plt.matshow(population, cmap=plt.cm.PuBu)
     plt.show()
 
-def simulate_multiple(epoches, case, infectivity=0.02):
+# Simulate multiple runs
+def simulate_multiple(epoches, case, infectivity=0.15, gathering_size=100):
     total_sum = np.array([])
     current_sum = np.array([])
     infected_maxes = []
     for k in range(epoches):
         total, current, time, population = simulate(case=case,
                                                 sqrt_population_size=100, time_of_sickness=14,
-                                                total_time_steps=200,
+                                                total_time_steps=400,
                                                 infectivity=infectivity,
                                                 global_susceptivity_mean=0.5, global_susceptivity_std=10,
                                                 local_susceptivity_mean=0.5, local_susceptivity_std=10,
                                                 initial_infected=0.05, 
                                                 total_neighbors=4,
-                                                gathering_size=100, gathering_probability=0.5,
+                                                gathering_size=gathering_size, gathering_probability=0.5,
                                                 gathering_frequency=1, gathering_number=4)
         
         infected_maxes.append(np.argmax(total))
@@ -224,10 +226,54 @@ def simulate_multiple(epoches, case, infectivity=0.02):
     print("Summaries:")
     print(np.mean(infected_maxes))
     print(total_sum[-1]/epoches)
+    return np.mean(infected_maxes), total_sum[-1]/epoches
 
+# Vary the infectivity
+def vary_infectivity(epoches, case=0):
+    infectivity_array = [0.02, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.5, 0.6, 0.8, 1]
+    inf_max_arr = []
+    inf_total_arr = []
+    for value in infectivity_array:
+        mx, tot= simulate_multiple(epoches, case=case, infectivity=value)
+        inf_max_arr.append(mx)
+        inf_total_arr.append(tot)
+
+    plt.subplot(2, 1, 1)
+    plt.scatter(infectivity_array, inf_max_arr)
+    plt.title('Infectivity vs Duration and Total')
+    plt.xlabel('Infectivity')
+    plt.ylabel('Duration')
+
+    plt.subplot(2, 1, 2)
+    plt.scatter(infectivity_array, inf_total_arr)
+    plt.xlabel('Infectivity')
+    plt.ylabel('Total')  
+    plt.show()
+
+# Vary the gathering size
+def vary_gathering_size(epoches, case=1):
+    gathering_size_arr = [0, 100, 200, 500, 1000, 5000, 10000]
+    inf_max_arr = []
+    inf_total_arr = []
+    for value in gathering_size_arr:
+        mx, tot= simulate_multiple(epoches, case=case, gathering_size=value)
+        inf_max_arr.append(mx)
+        inf_total_arr.append(tot)
+
+    plt.subplot(2, 1, 1)
+    plt.scatter(gathering_size_arr, inf_max_arr)
+    plt.title('Infectivity vs Duration and Total')
+    plt.xlabel('Gathering Size')
+    plt.ylabel('Duration')
+
+    plt.subplot(2, 1, 2)
+    plt.scatter(gathering_size_arr, inf_total_arr)
+    plt.xlabel('Gathering Size')
+    plt.ylabel('Total')  
+    plt.show()
 
 def main():
-    epoches = 1
+    epoches = 3
     random.seed(22)
 
     NO_GATHERING = 0
@@ -235,12 +281,15 @@ def main():
     MULTIPLE_REPEATED = 2
     MULTIPLE_RANDOM = 3
 
-    infectivity_array = [0.02, 0.05, 0.1, 0.2, 0.5, 1]
-    for value in infectivity_array:
-        simulate_multiple(epoches, NO_GATHERING, value)
+    # See how infectivity compares to pandemic duration and total number of cases
+    # vary_infectivity(epoches)
 
-    #simulate_multiple(epoches, NO_GATHERING)
-    #simulate_multiple(epoches, SINGLE_REPEATED)
+    # See how gathering size compares to pandemic duration and total number of cases
+    # at 1 gathering meeting at every 1 time step
+    #vary_gathering_size(epoches)
+
+    #simulate_multiple(epoches, case=NO_GATHERING)
+    #simulate_multiple(epoches, case=SINGLE_REPEATED)
     #simulate_multiple(epoches, MULTIPLE_REPEATED)
     #simulate_multiple(epoches, MULTIPLE_RANDOM)
     
